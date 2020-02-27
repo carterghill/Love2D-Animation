2487eafa44a0e64634d1138a754b02f3d18a33ab
Carter Hill <carterghill@gmail.com>
2020-02-27 16:36:24 -0600

    Added Animation.lua and test main.lua file

---
+++ main.lua
@@ -0,0 +1,17 @@
+function love.load()
+
+end
+
+function love.draw()
+
+end
+
+function love.update()
+
+end
+
+function love.keypressed(key, scancode, isrepeat)
+   if key == "escape" then
+      love.event.quit()
+   end
+end
